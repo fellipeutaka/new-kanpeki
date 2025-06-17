b87b7917cc8e4f9bd646ccd5c11086fe865ce92e
Fellipe Utaka <fellipeutaka@gmail.com>
2025-06-17 01:01:01 -0300

    feat: add link button example

--- src/components/ui/link/styles.ts
+++ src/components/ui/link/styles.ts
@@ -1,22 +1,27 @@
 import { cva } from "~/lib/cva";
 
 export const LinkStyles = cva({
-  base: [
-    "rounded-md current:font-normal outline-none transition-all",
-    "focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50",
-  ],
   variants: {
     variant: {
       unstyled: null,
       default: [
+        "rounded-md current:font-normal outline-none transition-all",
+        "focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50",
+
         "current:text-foreground text-muted-foreground",
         "hover:text-foreground",
       ],
       foreground: [
+        "rounded-md current:font-normal outline-none transition-all",
+        "focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50",
+
         "text-foreground",
         "hover:text-foreground/90 dark:hover:text-foreground/60",
       ],
       primary: [
+        "rounded-md current:font-normal outline-none transition-all",
+        "focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50",
+
         "text-primary",
         "hover:text-primary/90 dark:hover:text-primary/60",
       ],
--- src/demos/link-demo.tsx
+++ src/demos/link-demo.tsx
@@ -1,3 +1,5 @@
+import { ArrowRightIcon, Loader2Icon, SendIcon } from "lucide-react";
+import { ButtonStyles } from "~/components/ui/button";
 import { Link } from "~/components/ui/link";
 
 export function LinkDemo() {
@@ -14,11 +16,80 @@ export function LinkDemo() {
         <Link href="https://github.com/fellipeutaka" variant="unstyled">
           Unstyled
         </Link>
+      </div>
+      <div className="flex flex-wrap items-center gap-2 md:flex-row">
         <Link href="https://github.com/fellipeutaka" underline="always">
           Underline (Always)
         </Link>
         <Link href="https://github.com/fellipeutaka" underline="hover">
           Underline (Hover)
+        </Link>
+      </div>
+      <div className="flex flex-wrap items-center gap-2 md:flex-row">
+        <Link
+          href="https://github.com/fellipeutaka"
+          variant="unstyled"
+          className={ButtonStyles()}
+        >
+          Link Button
+        </Link>
+        <Link
+          href="https://github.com/fellipeutaka"
+          variant="unstyled"
+          className={ButtonStyles({ variant: "outline" })}
+        >
+          Link Button Outline
+        </Link>
+        <Link
+          href="https://github.com/fellipeutaka"
+          variant="unstyled"
+          className={ButtonStyles({ variant: "ghost" })}
+        >
+          Link Button Ghost
+        </Link>
+        <Link
+          href="https://github.com/fellipeutaka"
+          variant="unstyled"
+          className={ButtonStyles({ variant: "destructive" })}
+        >
+          Link Button Destructive
+        </Link>
+        <Link
+          href="https://github.com/fellipeutaka"
+          variant="unstyled"
+          className={ButtonStyles({ variant: "secondary" })}
+        >
+          Link Button Secondary
+        </Link>
+        <Link
+          href="https://github.com/fellipeutaka"
+          variant="unstyled"
+          className={ButtonStyles({ variant: "link" })}
+        >
+          Link Button Link
+        </Link>
+        <Link
+          href="https://github.com/fellipeutaka"
+          variant="unstyled"
+          className={ButtonStyles({ variant: "outline" })}
+        >
+          <SendIcon /> Send
+        </Link>
+        <Link
+          href="https://github.com/fellipeutaka"
+          variant="unstyled"
+          className={ButtonStyles({ variant: "outline" })}
+        >
+          Learn More <ArrowRightIcon />
+        </Link>
+        <Link
+          href="https://github.com/fellipeutaka"
+          variant="unstyled"
+          className={ButtonStyles({ variant: "outline" })}
+          isDisabled
+        >
+          <Loader2Icon className="animate-spin" />
+          Please wait
         </Link>
       </div>
     </div>
